5a1d515146a36b887d43c1bf7bca6f93427d499c
arman-yekkehkhani <arman.yekkehkhani@gmail.com>
2024-05-23 10:14:53 +0330

    feature(Main app): add main spring app

---
+++ src/test/java/com/arman/trueidentity/TrueIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arman.trueidentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueIdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
